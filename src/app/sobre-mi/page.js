3278e7eff579e2c9106e694c6230ea6b5489d89a
Agusilva7 <niupi7374@gmail.com>
2025-05-25 02:34:25 -0300

    change size mainbanner about

--- src/app/sobre-mi/page.js
+++ src/app/sobre-mi/page.js
@@ -9,7 +9,7 @@ export default function SobreMi(){
 
                 <div className="absolute inset-0 h-lvh bg-[#1c1c1c]/15 z-10" />
 
-                <section className="w-full h-[450px] md:[750px] xl:h-auto xl:w-1/4 text-black flex flex-col px-5 md:px-8 lg:px-20 xl:px-0 gap-1 lg:gap-2 justify-end xl:justify-center items-center text-center xl:text-justify z-20">
+                <section className="w-full h-[350px] md:[750px] xl:h-auto xl:w-1/4 text-black flex flex-col px-5 md:px-8 lg:px-20 xl:px-0 gap-1 lg:gap-2 justify-end xl:justify-center items-center text-center xl:text-justify z-20">
                     <h1 className="text-2xl md:text-3xl lg:text-4xl">Hola Soy Agustin Silva</h1>
                     <h2 className="text-[19px] md:text-2xl">Apasionado por el desarrollo web</h2>
                     <p>Soy <b>desarrollador frontend</b> con enfoque en diseño funcional, rendimiento y experiencia de usuario. Me especializo en crear interfaces modernas, limpias y eficientes que aporten valor real a cada proyecto. Actualmente vivo en <b>General Rodríguez, Buenos Aires</b>, y me apasiona construir soluciones digitales.</p>
